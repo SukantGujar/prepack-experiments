228eca3bdd0b166cef4e3d32e1d8426bdfc9e5c8
Sukant Gujar <sukantgujar@yahoo.com>
2017-05-15 12:47:37 +0530

    Conditionalize prepack plugin in build.

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,28 @@ const webpack = require('webpack'),
  *
  */
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+	plugins = [];
+
+if (process.env.WITH_PREPACK) {
+	plugins.push(
+		new PrepackWebpackPlugin({
+			prepack: {
+				logStatistics: true,
+				//trace: true,
+				//logModules: true,
+				//internalDebug: true
+			}
+		})
+	);
+}
 
 module.exports = {
 	entry: './index.js',
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve('./dist'),
-		library: "prepack-experiments.logger",
+		library: ["prepack-experiments", "logger"],
 		libraryTarget: "umd"
 	},
 
@@ -34,20 +48,5 @@ module.exports = {
 		]
 	},
 
-	plugins: [
-		//new UglifyJSPlugin()
-		// new LodashModuleReplacementPlugin({
-		// 	'collections': true,
-		// 	'paths': true
-		// }),
-
-		new PrepackWebpackPlugin({
-			prepack: {
-				logStatistics: true,
-				//trace: true,
-				//logModules: true,
-				//internalDebug: true
-			}
-		})
-	]
+	plugins: plugins
 };
